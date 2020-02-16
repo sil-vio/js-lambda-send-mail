1667ee1f5f1d9a3be572c9402f7709e4488f5020
Silvio Giannini <giannini.silvio@gmail.com>
2020-02-16 11:35:42 +0100

    feature: send mail from lambda

---
+++ functions/send-mail/send-mail.js
@@ -0,0 +1,54 @@
+var nodemailer = require('nodemailer');
+
+const mail = process.env.MAIL;
+const pwd = process.env.PWD;
+
+var transport = nodemailer.createTransport("SMTP", {
+	service: "hotmail",
+	auth: {
+		user: mail,
+		pass: pwd
+	}
+});
+// exports.handler is required by netlify to process.
+exports.handler = async (event, context, callback) => {
+	// Only allow POST
+	const headers = {
+		'Access-Control-Allow-Origin': '*',
+		'Access-Control-Allow-Headers':
+			'Origin, X-Requested-With, Content-Type, Accept',
+	};
+	if (event.httpMethod === 'OPTIONS') {
+		return {
+			headers,
+			statusCode: 200,
+			body: JSON.stringify({ message: 'You can use CORS' }),
+		}
+	}
+	if (event.httpMethod !== "POST") {
+		return { statusCode: 405, body: "Method Not Allowed" };
+	}
+
+	// setup e-mail data, even with unicode symbols
+	var mailOptions = {
+		from: mail, // sender address (who sends)
+		to: mail, // list of receivers (who receives)
+		subject: 'Nuovo Messaggio : ', // Subject line
+		text: 'Hello world ', // plaintext body
+		html: '<b>Hello world </b><br> This is the first email sent with Nodemailer in Node.js' // html body
+	};
+
+	// send mail with defined transport object
+	transporter.sendMail(mailOptions, function (error, info) {
+		if (error) {
+			return console.log(error);
+		}
+
+		console.log('Message sent: ' + info.response);
+	});
+	return {
+		headers,
+		statusCode: 201,
+		body: `message saved`
+	};
+}
